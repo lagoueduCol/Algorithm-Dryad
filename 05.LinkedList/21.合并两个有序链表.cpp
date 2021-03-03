3666b39e73fdbb02d493e44296e1288f418dc615
Druid <lagou@com>
2021-03-03 22:33:35 +0800

    update 24
    Signed-off-by: Druid <lagou@com>

---
+++ 05.LinkedList/21.合并两个有序链表.cpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=cpp
+ *
+ * [21] 合并两个有序链表
+ *
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (65.18%)
+ * Likes:    1499
+ * Dislikes: 0
+ * Total Accepted:    457.5K
+ * Total Submissions: 702K
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 
+ * l1 和 l2 均按 非递减顺序 排列
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+
+    }
+};
+// @lc code=end
+
---
+++ 05.LinkedList/21.合并两个有序链表.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode.cn id=21 lang=python
+#
+# [21] 合并两个有序链表
+#
+# https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+#
+# algorithms
+# Easy (65.18%)
+# Likes:    1499
+# Dislikes: 0
+# Total Accepted:    457.5K
+# Total Submissions: 702K
+# Testcase Example:  '[1,2,4]\n[1,3,4]'
+#
+# 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：l1 = [1,2,4], l2 = [1,3,4]
+# 输出：[1,1,2,3,4,4]
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：l1 = [], l2 = []
+# 输出：[]
+# 
+# 
+# 示例 3：
+# 
+# 
+# 输入：l1 = [], l2 = [0]
+# 输出：[0]
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 两个链表的节点数目范围是 [0, 50]
+# -100 
+# l1 和 l2 均按 非递减顺序 排列
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+# @lc code=end
+

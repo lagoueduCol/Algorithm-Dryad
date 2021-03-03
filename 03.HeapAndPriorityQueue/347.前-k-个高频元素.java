f02ae676abf0608507383bf528076b157b4dcae2
Druid <lagou@com>
2021-03-03 14:05:37 +0800

    update code
    Signed-off-by: Druid <lagou@com>

---
+++ 03.HeapAndPriorityQueue/347.前-k-个高频元素.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=347 lang=java
+ *
+ * [347] 前 K 个高频元素
+ *
+ * https://leetcode-cn.com/problems/top-k-frequent-elements/description/
+ *
+ * algorithms
+ * Medium (61.87%)
+ * Likes:    653
+ * Dislikes: 0
+ * Total Accepted:    136.4K
+ * Total Submissions: 220.1K
+ * Testcase Example:  '[1,1,1,2,2,3]\n2'
+ *
+ * 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums = [1,1,1,2,2,3], k = 2
+ * 输出: [1,2]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums = [1], k = 1
+ * 输出: [1]
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+ * 你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+ * 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。
+ * 你可以按任意顺序返回答案。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+    }
+}
+// @lc code=end
+
---
+++ 03.HeapAndPriorityQueue/692.前k个高频单词.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=692 lang=java
+ *
+ * [692] 前K个高频单词
+ *
+ * https://leetcode-cn.com/problems/top-k-frequent-words/description/
+ *
+ * algorithms
+ * Medium (52.47%)
+ * Likes:    214
+ * Dislikes: 0
+ * Total Accepted:    24.3K
+ * Total Submissions: 46.2K
+ * Testcase Example:  '["i", "love", "leetcode", "i", "love", "coding"]\n2'
+ *
+ * 给一非空的单词列表，返回前 k 个出现次数最多的单词。
+ * 
+ * 返回的答案应该按单词出现频率由高到低排序。如果不同的单词有相同出现频率，按字母顺序排序。
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+ * 输出: ["i", "love"]
+ * 解析: "i" 和 "love" 为出现次数最多的两个单词，均为2次。
+ * ⁠   注意，按字母顺序 "i" 在 "love" 之前。
+ * 
+ * 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"],
+ * k = 4
+ * 输出: ["the", "is", "sunny", "day"]
+ * 解析: "the", "is", "sunny" 和 "day" 是出现次数最多的四个单词，
+ * ⁠   出现次数依次为 4, 3, 2 和 1 次。
+ * 
+ * 
+ * 
+ * 
+ * 注意：
+ * 
+ * 
+ * 假定 k 总为有效值， 1 ≤ k ≤ 集合元素数。
+ * 输入的单词均由小写字母组成。
+ * 
+ * 
+ * 
+ * 
+ * 扩展练习：
+ * 
+ * 
+ * 尝试以 O(n log k) 时间复杂度和 O(n) 空间复杂度解决。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+
+    }
+}
+// @lc code=end
+

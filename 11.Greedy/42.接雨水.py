a6698e9f28aa2367932751a00f2040f64a62b5e6
Druid <lagou@com>
2021-03-09 22:03:03 +0800

    update 42
    Signed-off-by: Druid <lagou@com>

---
+++ 11.Greedy/42.接雨水.py
@@ -0,0 +1,57 @@
+#
+# @lc app=leetcode.cn id=42 lang=python
+#
+# [42] 接雨水
+#
+# https://leetcode-cn.com/problems/trapping-rain-water/description/
+#
+# algorithms
+# Hard (54.56%)
+# Likes:    2115
+# Dislikes: 0
+# Total Accepted:    205K
+# Total Submissions: 375.4K
+# Testcase Example:  '[0,1,0,2,1,0,1,3,2,1,2,1]'
+#
+# 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 
+# 
+# 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# 输出：6
+# 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：height = [4,2,0,3,2,5]
+# 输出：9
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# n == height.length
+# 0 
+# 0 
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+# @lc code=end
+

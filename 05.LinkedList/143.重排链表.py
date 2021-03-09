e114c925edfc05daa8f9a1da16937d25de650c0e
Druid <lagou@com>
2021-03-09 21:18:26 +0800

    update code
    Signed-off-by: Druid <lagou@com>

---
+++ 05.LinkedList/143.重排链表.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode.cn id=143 lang=python
+#
+# [143] 重排链表
+#
+# https://leetcode-cn.com/problems/reorder-list/description/
+#
+# algorithms
+# Medium (59.50%)
+# Likes:    500
+# Dislikes: 0
+# Total Accepted:    77.9K
+# Total Submissions: 130.9K
+# Testcase Example:  '[1,2,3,4]'
+#
+# 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+# 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+# 
+# 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+# 
+# 示例 1:
+# 
+# 给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+# 
+# 示例 2:
+# 
+# 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+        
+# @lc code=end
+

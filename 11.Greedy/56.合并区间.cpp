449c5bee574d7c3cb4b3a66c31b457838063a7ab
Druid <lagou@com>
2021-03-09 21:33:10 +0800

    update 56
    Signed-off-by: Druid <lagou@com>

---
+++ 11.Greedy/56.合并区间.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=56 lang=cpp
+ *
+ * [56] 合并区间
+ *
+ * https://leetcode-cn.com/problems/merge-intervals/description/
+ *
+ * algorithms
+ * Medium (44.51%)
+ * Likes:    827
+ * Dislikes: 0
+ * Total Accepted:    198.6K
+ * Total Submissions: 445.4K
+ * Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
+ *
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi]
+ * 。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * 输出：[[1,6],[8,10],[15,18]]
+ * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：intervals = [[1,4],[4,5]]
+ * 输出：[[1,5]]
+ * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * intervals[i].length == 2
+ * 0 i i 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+    }
+};
+// @lc code=end
+
